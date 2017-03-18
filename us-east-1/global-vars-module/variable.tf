4c466927de3b2e821c96ce3779384ba51858fd9d
Marcio Godoi <souzagodoi@gmail.com>
2017-03-18 00:56:16 -0300

    Adding global-variables folder which is used to store global variables to this project

---
+++ us-east-1/global-vars-module/variable.tf
@@ -0,0 +1,3 @@
+variable "default-region" { default = "us-east-1" }
+variable "terraform-state-bucket" { default = "example-s3-bucket-name" }
+variable "terraform-state-bucket-base-path" { default = "key=base" }
