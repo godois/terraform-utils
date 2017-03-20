953955ee9e91c5481e350de10f4b3ebfb2aedbb2
Marcio Godoi <souzagodoi@gmail.com>
2017-03-19 22:26:00 -0300

    some corrections

---
+++ us-east-1/global-vars-module/output.tf
@@ -0,0 +1,3 @@
+output "default-region" { value = "${var.default-region}" }
+output "terraform-state-bucket" { value = "${var.terraform-state-bucket}" }
+output "terraform-state-bucket-base-path" { value = "${var.terraform-state-bucket-base-path}" }
