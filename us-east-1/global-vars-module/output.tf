b55ac7d16308ec8fb9749bca588188abf97e4570
Marcio Godoi <souzagodoi@gmail.com>
2017-03-23 22:03:17 -0300

    Adding new variables used in ec2 lauching instances

--- us-east-1/global-vars-module/output.tf
+++ us-east-1/global-vars-module/output.tf
@@ -1,3 +1,5 @@
 output "default-region" { value = "${var.default-region}" }
 output "terraform-state-bucket" { value = "${var.terraform-state-bucket}" }
 output "terraform-state-bucket-base-path" { value = "${var.terraform-state-bucket-base-path}" }
+output "ubuntu-stable-ami" { value = "${var.ubuntu-stable-ami}" }
+output "instance-key-name" { value = "${var.instance-key-name}"}
--- us-east-1/global-vars-module/variable.tf
+++ us-east-1/global-vars-module/variable.tf
@@ -1,3 +1,5 @@
 variable "default-region" { default = "us-east-1" }
 variable "terraform-state-bucket" { default = "example-s3-bucket-name" }
 variable "terraform-state-bucket-base-path" { default = "key=base" }
+variable "ubuntu-stable-ami" { default = "ami-f4cc1de2" }
+variable "instance-key-name" { default = "ubuntu-example" }
